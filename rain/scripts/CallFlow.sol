0fd9e6ec681dfc239ed264aad9a3ee91f502b031
Matt Gabrenya <matt@buildyourweb.app>
2024-03-12 16:45:06 -0700

    progress on debugging deposit call -- switch order of erc20 flow arguments

--- rain/scripts/CallFlow.sol
+++ rain/scripts/CallFlow.sol
@@ -13,22 +13,36 @@ import {EvaluableV2} from "rain.interpreter.interface/lib/caller/LibEvaluable.so
 import {SignedContextV1, EvaluableConfigV3} from "rain.interpreter.interface/interface/IInterpreterCallerV2.sol";
 import {IInterpreterStoreV2} from "rain.interpreter.interface/interface/IInterpreterStoreV2.sol";
 import {IInterpreterV2} from "rain.interpreter.interface/interface/IInterpreterV2.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 
 contract CloneFlow is Script {
     function run() public {
-      string memory POLYGON_RPC_URL = vm.envString("POLYGON_RPC_URL");
-      uint256 forkId = vm.createFork(POLYGON_RPC_URL);
-      vm.startPrank(0x8d0F52e2A9d9fF7F4a6952C32552dEAd4f732DD5);
-      vm.selectFork(forkId);
-
       address RAIN_INTERPRETER_ADDRESS = 0x8bb0e1Ade233f386668f6e3c11762f18bF8293b3;
       address RAIN_STORE_ADDRESS = 0xCCe6D0653B6DAC3B5fAd3F2A8E47cCE537126aD0;
-      address depositExpression = 0xd36E23f324480AaE6A42Ee4490fEf3FD03bDBfeE;
 
-      EvaluableV2 memory evaluableDeposit = EvaluableV2(IInterpreterV2(RAIN_INTERPRETER_ADDRESS), IInterpreterStoreV2(RAIN_STORE_ADDRESS), depositExpression);
-      Flow flow = Flow(0x0b4af3781FA212E2E4BaB6bA1086fab1FA48d225);
+      address FLOW_CLONE_ADDRESS = 0xE12d142a226E0C7096039cebbaCB4dff625Dd8a3;
+      address DEPOSIT_EXPRESSION_ADDRESS = 0xf7F99860Ee1D60ca5981d53A342e5B1BF68032E2;
+      
+      address MY_WALLET_ADDRESS = 0x8d0F52e2A9d9fF7F4a6952C32552dEAd4f732DD5;
+      uint256 AMOUNT = 100;      
+
+      string memory POLYGON_RPC_URL = vm.envString("POLYGON_RPC_URL");
+      uint256 forkId = vm.createFork(POLYGON_RPC_URL);
+      vm.startPrank(MY_WALLET_ADDRESS);
+      vm.selectFork(forkId);
+
+      // Confirm my wallet has granted allowance to flow clone contract
+      IERC20 paymentToken = IERC20(0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359);
+      uint256 allowance = paymentToken.allowance(MY_WALLET_ADDRESS, FLOW_CLONE_ADDRESS);
+      console.log("allowance", allowance);
+      uint256 balance = paymentToken.balanceOf(MY_WALLET_ADDRESS);
+      console.log("balance", balance);
+
+      // Call deposit on flow clone
+      EvaluableV2 memory evaluableDeposit = EvaluableV2(IInterpreterV2(RAIN_INTERPRETER_ADDRESS), IInterpreterStoreV2(RAIN_STORE_ADDRESS), DEPOSIT_EXPRESSION_ADDRESS);
+      Flow flow = Flow(FLOW_CLONE_ADDRESS);
       uint256[] memory callerContext = new uint256[](1);
-      callerContext[0] = 1;
+      callerContext[0] = AMOUNT;
       SignedContextV1[] memory signedContext = new SignedContextV1[](0);
       
       
